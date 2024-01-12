925f18bd8b846feccaab3b7581fe3bcc4a5aa210
davemac93 <112555280+davemac93@users.noreply.github.com>
2024-01-12 18:35:44 +0100

    Add files via upload

---
+++ Frontend/src/App.js
@@ -0,0 +1,28 @@
+  import Home from "./pages/Home";
+  import Product from "./pages/Product";
+  import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+  } from "react-router-dom";
+  import Products from "./pages/Products";
+  import Protected from "./pages/Protected";
+
+  function App() {
+    console.log("ab");
+    const user = false;
+    return (
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/product/:id" element={<Product />}></Route>
+          <Route path="/products/:category" element={<Products />}></Route>
+          <Route path="/products/" element={<Products />}></Route>
+          <Route path="/prod/" element={<Protected />}></Route>
+        </Routes>
+      </Router>
+    );
+  }
+
+  export default App;
---
+++ Frontend/src/data.js
@@ -0,0 +1,31 @@
+  import axios from "axios";
+  import cheerio from 'cheerio';
+
+  export const sliderItems = [
+    {
+      id: 1,
+      img: "./Laptopy.jpg",
+      title: "LAPTOPY",
+      desc: "NAJLEPSZE CENY.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 0,
+      name: "Laptopy",
+      url: 'https://www.x-kom.pl/g-2/c/159-laptopy-notebooki-ultrabooki.html',
+      pageCount: 2,
+    },
+  ];
+
+  export const products = [
+    {
+      id: 1,
+      name: "1",
+      price: 10,
+      img: "../photo2.jpg",
+    },
+  ];
+
