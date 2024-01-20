863d1a387465e65b273402cb21abf8d243b9e66a
davemac93 <112555280+davemac93@users.noreply.github.com>
2024-01-20 10:29:50 +0100

    Navbar + Slider frontend

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,5 +1,8 @@
   import Home from "./pages/Home";
   import Product from "./pages/Product";
+  import Register from "./pages/Register";
+  import Login from "./pages/Login";
+  import Cart from "./pages/Cart";
   import {
     BrowserRouter as Router,
     Routes,
