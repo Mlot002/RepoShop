57f1de9829079a2169b38a50c5375f88d894be9f
SzymonWu <126473845+SzymonWu@users.noreply.github.com>
2024-01-26 17:49:26 +0100

    Serach Bar + sortowanie po cenie

--- Frontend/src/components/ProductList.jsx
+++ Frontend/src/components/ProductList.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from "react";
-import styled from "styled-components";
-import Product from "./Product";
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const Container = styled.div`
   width: 80%;
@@ -11,43 +11,11 @@ const Container = styled.div`
   justify-content: left;
 `;
 
-const ProductList = ({ cat, filters, sort }) => {
-  const [apiProducts, setApiProducts] = useState([]);
-
-  useEffect(() => {
-    const getProducts = async () => {
-      try {
-        const res = await axios.get(
-           "https://localhost:7080/api/Products/" + cat
-        );
-        console.log(cat);
-        setApiProducts(res.data);
-        console.log(res.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    getProducts();
-  }, [cat]);
-
-  console.log(apiProducts);
-
-  return (
-    <Container>
-      {apiProducts.map((item) => (
-        <ProductContainer key={item._id}>
-          <ProductImage src={item.image} alt={item.name} />
-          <ProductName>{item.name}</ProductName>
-          <ProductPrice>{item.price}</ProductPrice>
-        </ProductContainer>
-      ))}
-    </Container>
-  );
-};
-
-const ProductContainer = styled.div`
+const ProductContainer = styled(Link)`
   width: 200px;
   margin: 10px;
+  text-decoration: none;
+  color: inherit;
 `;
 
 const ProductImage = styled.img`
@@ -61,5 +29,53 @@ const ProductName = styled.h3`
 `;
 
 const ProductPrice = styled.p``;
+
+const ProductList = ({ cat, filters, sort }) => {
+  const [apiProducts, setApiProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await axios.get(`https://localhost:7080/api/Products/${cat}`);
+        setApiProducts(res.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getProducts();
+  }, [cat]);
+
+  const filteredProducts = apiProducts.filter((item) =>
+    item.name.trim().toLowerCase().includes(filters.search.trim().toLowerCase())
+  );
+
+  const sortedProducts = sortProducts(filteredProducts, sort);
+
+  return (
+    <Container>
+      {sortedProducts.map((item) => (
+        <ProductContainer key={item.id} to={`/product/${cat}/${item.id}`}>
+          <ProductImage src={item.image} alt={item.name} />
+          <ProductName>{item.name}</ProductName>
+          <ProductPrice>{item.price.replace(/\s/g, '')}</ProductPrice>
+        </ProductContainer>
+      ))}
+    </Container>
+  );
+};
+
+const sortProducts = (products, sort) => {
+
+  switch (sort) {
+    case 'lowest':
+      return [...products].sort((a, b) => parseFloat(a.price.replace(/\s/g, '')) - parseFloat(b.price.replace(/\s/g, '')));
+    case 'highest':
+      return [...products].sort((a, b) => parseFloat(b.price.replace(/\s/g, '')) - parseFloat(a.price.replace(/\s/g, '')));
+    case 'default':
+        return products;
+    default:
+        return products;
+  }
+};
 
 export default ProductList;
